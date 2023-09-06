7cb612e2704687cc25548b38b2cb05019a4a33ef
Pallavi-0880 <pallavithakur0880@gmail.com>
2023-09-06 16:46:04 +0530

    DOM Event

--- sketch.js
+++ sketch.js
@@ -1,47 +1,60 @@
 var divid = document.querySelector("#box1");
-divid.style.backgroundColor = "yellow";
-divid.style.fontSize = "10px";
+divid.style.backgroundColor = "white";
+//divid.style.fontSize = "10px";
 
 var divid = document.querySelector("#box3");
-divid.style.backgroundColor = "brown";
-divid.style.fontSize = "10px";
+divid.style.backgroundColor = "white";
+//divid.style.fontSize = "10px";
 
 
 var divid = document.querySelector("#box6");
-divid.style.backgroundColor = "red";
-divid.style.fontSize = "10px";
+divid.style.backgroundColor = "white";
+//divid.style.fontSize = "10px";
 
 var divid = document.querySelector("#box9");
-divid.style.backgroundColor = "green";
-divid.style.fontSize = "10px";
+divid.style.backgroundColor = "white";
+//divid.style.fontSize = "10px";
 
 var divid = document.querySelector("#box12");
-divid.style.backgroundColor = "pink";
-divid.style.fontSize = "10px";
+divid.style.backgroundColor = "white";
+//divid.style.fontSize = "10px";
 
 var divid = document.querySelector("#box14");
-divid.style.backgroundColor = "red";
+divid.style.backgroundColor = "white";
 
 var divid = document.querySelector("#box16");
-divid.style.backgroundColor = "purple";
+divid.style.backgroundColor = "white";
 
 var divid = document.querySelector("#box8");
-divid.style.backgroundColor = "yellow";
+divid.style.backgroundColor = "White";
 
 
 
-var parent = document.querySelector(".parent");
-parent.setAttribute("id","box");
-parent.style.backgroundColor = "blue";
-console.log(parent.getAttribute ("class"));
+ var parent = document.querySelector(".parent");
+ parent.setAttribute("id","box");
+ parent.style.backgroundColor = "white";
+ console.log(parent.getAttribute ("class"));
 
-var newDiv = document.createElement("div");
-newDiv.classList.add("divid");
-newDiv.textContent = "Abc";
-parent.appendChild(newDiv);
+// var newDiv = document.createElement("div");
+// newDiv.classList.add("divid");
+// newDiv.textContent = "Abc";
+// parent.appendChild(newDiv);
 
-var total = document.querySelectorAll(".divid");
-console.log(total);
+ var total = document.querySelectorAll(".divid");
+ console.log(total);
+
+
+//------------------------------DOM Event--------------------------------
+//divid.addEventListener('click',changeToBlack);
+//function changeToBlack(){
+ //   divid.style.backgroundColor = "grey";
+//}
+
+total.forEach((div)=>{
+    div.addEventListener('click',()=>{
+        div.style.backgroundColor = "grey";
+    })
+})
 
 
 
